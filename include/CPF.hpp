7c05994cec62a31c939049050b2cca2756c02352
Bielzada444 <241038942@aluno.unb.br>
2025-04-07 16:59:57 -0300

    Update CPF.hpp
    Voltando ao original que tava certo

--- include/CPF.hpp
+++ include/CPF.hpp
@@ -1,26 +1,26 @@
 #ifndef CPF_HPP
-#define CPF_HPP
-
-#include <string>
-#include <stdexcept> // biblioteca onde esta contida a  calsse base runtime_error 
-
-class DominioException : public std::runtime_error { // classe publica de tratamento de erros
-    public:
-        explicit DominioException(const std::string& mensagem) // construtor da classe que recebe as mensagens de erro
-        : std::runtime_error(mensagem) {} // passa a mensagem para o construtor da classe base
-};
-
-class CPF {
-private:
-	std::string NumeroCpf;
-	bool Validar(const std::string& CpfInserido);
-	bool ValidarDigitosVerificadores(const std::string& Cpf);
-public:
-	void SetCPF(const std::string& NovoCpf); // "escreve/muda" o conteudo do atributo NumeroCpf
-	inline std::string GetCPF() const { 
-	    return NumeroCpf; 
-	} // "retorna" uma copia do atributo NumeroCpf
-};
-
-
-#endif
+ #define CPF_HPP
+ 
+ #include <string>
+ #include <stdexcept> // biblioteca onde esta contida a  calsse base runtime_error 
+ 
+ class DominioException : public std::runtime_error { // classe publica de tratamento de erros
+     public:
+         explicit DominioException(const std::string& mensagem) // construtor da classe que recebe as mensagens de erro
+         : std::runtime_error(mensagem) {} // passa a mensagem para o construtor da classe base
+ };
+ 
+ class CPF {
+ private:
+ 	std::string numeroCpf;
+ 	bool validar(const std::string& cpfInserido);
+ 	bool validarDigitosVerificadores(const std::string& cpf);
+ public:
+ 	void setCPF(const std::string& novoCpf); // "escreve/muda" o conteudo do atributo numeroCpf
+ 	inline std::string getCPF() const { 
+ 	    return numeroCpf; 
+ 	} // "retorna" uma copia do atributo numeroCpf
+ };
+ 
+ 
+ #endif
