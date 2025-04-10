c166c75fe3903f2b41a35d4d741126089f4fcd05
henrique505 <henriquedeeealmeida@gmail.com>
2025-04-10 15:45:14 -0300

    Update Senha.h

--- include/Senha.h
+++ include/Senha.h
@@ -11,10 +11,10 @@ class Senha{
     private:
         static const int CARACTERES = 6;        //a senha deve possuir 6 caracteres
         std::string senha;                      //a senha é lida como uma string
-        bool validaSenha(std::string);          //retorna true se a senha for valida e false se nao for
+        bool validaSenha(std::string senha);          //retorna true se a senha for valida e false se nao for
 
     public:
-        bool setSenha(std::string);             //inicializa a senha de acordo input
+        bool setSenha(std::string senha);             //inicializa a senha de acordo input
         std::string getSenha() const;           //retorna a string senha
 };
 
