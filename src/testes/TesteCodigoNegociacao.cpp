574a503432fbb7328d7533c6734450ee939cae9d
Bielzada444 <241038942@aluno.unb.br>
2025-04-10 17:20:54 -0300

    Update TesteCodigoNegociacao.cpp
    Código temporario de reformulação para implementação do Tratamento de erro

--- src/testes/TesteCodigoNegociacao.cpp
+++ src/testes/TesteCodigoNegociacao.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <cassert>
 #include "CodigoNegociacao.hpp"
+#include <string>
 
-class TesteCodigoNegociacao {
+/*class TesteCodigoNegociacao {
 
 public:
     void testarCodigoValido() {
@@ -17,13 +18,13 @@ public:
     void testarCodigoInvalido() {
         CodigoNegociacao c;
         try {
-            c.setCodigo("###"); // verifica lançamento de excessao
+            c.setCodigo("###"); // verifica lanï¿½amento de excessao
             assert(false);
         } catch (const std::invalid_argument&) {
             assert(true);
         }
         try {
-            c.setCodigo("Testar Meu codigo"); // verifica lançamento de excessao
+            c.setCodigo("Testar Meu codigo"); // verifica lanï¿½amento de excessao
             assert(false);
         } catch (const std::length_error&) {
             assert(true);
@@ -35,10 +36,24 @@ public:
         testarCodigoInvalido();
         std::cout << "Todos os testes passaram!\n";
     }
-};
+};*/
 
 int main() {
-    TesteCodigoNegociacao teste;
-    teste.rodarTodosTestes();
+    CodigoNegociacao codigo;//  objeto cpf da classe CPF
+    std::string input; // c string input para armazenar o CPF
+    bool valido = false; // bool para controle do loop
+    
+    while(!valido){
+        std::cout << "Digite o Codigo: " << std::endl;
+        std::cin >> input; // atribuicao do CPF digitado
+       try{ // tentativa da operacao do bloco
+            codigo.setCodigo(input); // envio para validacao
+            std::cout << "CPF valido: " << input << std::endl;
+            valido = true;
+        } catch (const DominioException& cpfError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
+        }
+        
+    }
     return 0;
 }
