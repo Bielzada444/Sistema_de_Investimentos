0a46e06bec89285f103795f49383b1b4bff7fa83
Bielzada444 <241038942@aluno.unb.br>
2025-04-11 16:05:33 -0300

    Update TestesSenha.cpp
    Correção do TestesSenha para atender a implementação do tratamento de exceções

--- src/testes/TestesSenha.cpp
+++ src/testes/TestesSenha.cpp
@@ -1,95 +1,79 @@
 //
-// Criado por Nyvea em 08/04/2025.
+// Criado por Nyvea em 11/04/2025.
 // Matricula: 241040207
 
 #include <iostream>
-#include "senha.h"
+#include "Senha.h"
 
 int main()
 {
-    Senha a;                                                        //cria objeto tipo senha
-    std::string senha = "56gH&n";                                   //atribui um valor a uma string (no caso, senha valida)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';        //se a senha for valida
+    Senha a;                                                                         //cria objeto tipo senha
+    std::string senha = "56gH&n";                                                //atribui um valor a uma string (no caso, senha valida)
+    try{
+    a.setSenha(senha);
+    std::cout << "Senha valida: " << senha << std::endl;
+    } catch (const DominioException& senhaError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
     }
-    else{
-        std::cout << "A senha inserida eh invalida!\n";             //se a senha for invalida
+    senha = "m%12s5";
+    
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-
-    senha = "m%12s5";                                               //ex senha invalida (sem letra maiuscula)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
+    senha = "JK&67%";
+    
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
+    senha = "KmSt#&";
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-
-    senha = "JK&67%";                                               //ex senha invalida (sem letra minuscula)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
+    senha = "JKmtS9";
+    
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
+    senha = "Tsjs#5";
+    
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-
-    senha = "KmSt#&";                                               //ex senha invalida (sem numero)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
+    senha = "Nj7#SQt%";
+    
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
+    
+    senha = "7$Sn";
+    try{
+    a.setSenha(senha);
+    } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
     }
-
-    senha = "JKmtS9";                                               //ex senha invalida (sem caracter especial)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
-    }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
-    }
-
-    senha = "Tsjs#5";                                               //ex senha invalida (caracter duplicado)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
-    }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
-    }
-
-    senha = "Nj7#SQt%";                                             //ex senha invalida (maior que 6 caracteres)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
-    }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
-    }
-
-    senha = "7$Sn";                                                 //ex senha invalida (menor que 6 caracteres)
-
-    if(a.setSenha(senha)){
-        std::cout << "A senha eh: " << a.getSenha() << '\n';
-    }
-    else{
-        std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
-    }
-
+    
     std::cout << "Digite a senha para teste (para terminar o programa, digite '0'\n";               //para mais testes
     std::getline(std::cin, senha);
 
     while(senha.compare("0")){
 
-        if(a.setSenha(senha)){
-            std::cout << "A senha eh: " << a.getSenha() << '\n';
-        }
-        else{
-            std::cout << "A senha inserida (" << senha << ") eh invalida!\n";
+        try{
+            a.setSenha(senha);
+            std::cout << "Senha valida: " << senha << std::endl;
+        } catch (const DominioException& senhaError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << senhaError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
         }
 
         std::cout << "Digite a senha para teste (para terminar o programa, digite '0')\n";
