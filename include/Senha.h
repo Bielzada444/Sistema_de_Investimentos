68995470777d561bd219692024268706fb8d5493
Bielzada444 <241038942@aluno.unb.br>
2025-04-11 16:08:38 -0300

    Update Senha.h
    Implementação da classe para execução de tratamentos de exceções

--- include/Senha.h
+++ include/Senha.h
@@ -1,12 +1,19 @@
 //
-// Criado por Nyvea em 08/04/2025.
+// Criado por Nyvea em 11/04/2025.
 // Matricula: 241040207
 
 #ifndef SENHA_H_INCLUDED
 #define SENHA_H_INCLUDED
 
 #include <string>
+#include <stdexcept>
 
+class DominioException : public std::runtime_error { // classe publica de tratamento de erros
+     public:
+         explicit DominioException(const std::string& mensagem) // construtor da classe que recebe as mensagens de erro
+         : std::runtime_error(mensagem) {} // passa a mensagem para o construtor da classe base
+ };
+ 
 class Senha{
     private:
         static const int CARACTERES = 6;        //a senha deve possuir 6 caracteres
