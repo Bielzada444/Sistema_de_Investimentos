71485c608db42bffe196a89d461beb349f3a2fcd
henrique505 <henriquedeeealmeida@gmail.com>
2025-04-14 14:36:48 -0300

    Update TestesQuantidade.cpp
    alteracao na forma em que o input do usuario eh lida, ateracao de std::cin para std::getline que lida melhor com espacos na string

--- src/testes/TestesQuantidade.cpp
+++ src/testes/TestesQuantidade.cpp
@@ -11,14 +11,12 @@ int main(){
     
     while(!valido){
            std::cout << "Digite a quantidade: ";
-        std::cin >> input; // Atribuicao da quantidade digitado
+        std::getline(std::cin, input); // Atribuicao da quantidade digitado
        try{ // Tentativa da operacao do bloco
             qtd.setQuantidade(input); // envio para validacao
             std::cout << "Quantidade valida: " << qtd.getQuantidade() << std::endl;
             valido = true;
-        }     catch (const std::out_of_range& qtdError) { // Lanca uma excecao para para valores fora da faixa permitida pelo stoi
-    std::cerr << "O valor digitado deve estar entre 0 e 1,000,000\n";
-  } 
+       }
         catch (const DominioException& qtdError) { // No caso de erro, executado 
            std::cerr << "Erro de validacao: " << qtdError.what() << std::endl; // No caso de excecao, exibe mens de erro contida em DE
 
