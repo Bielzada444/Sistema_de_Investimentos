092da2099a524c8a547b27ef61f49cc28023df86
Bielzada444 <241038942@aluno.unb.br>
2025-04-14 15:07:18 -0300

    Update and rename TestesCPF.cpp to TestesCpf.cpp
    Adição dos casos de testes

---
+++ src/testes/TestesCpf.cpp
@@ -0,0 +1,76 @@
+//
+// Criado por Gabriel em 03/04/2025.
+// Matricula: 241038942
+
+#include <iostream>
+#include "Cpf.hpp"
+
+int main()
+{
+    
+    CPF cpf; //  objeto cpf da classe CPF
+    std::string cpfinput = "10145408108"; //  string cpfinput para armazenar o CPF
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    cpfinput = "123";
+    
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    cpfinput = "11111111111";
+    
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    cpfinput = "123.456.789-09";
+    
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    cpfinput = "52998224724";
+    
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    cpfinput = "1234567890a";
+    
+    try{
+    cpf.setCPF(cpfinput);
+    std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+    } catch (const DominioException& cpfError) {                                  // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl;    // no caso de excecao, exibe mens de erro contida em DE 
+    }
+    
+    bool valido = false; // bool para controle do loop
+    
+    while(!valido){
+        std::cout << "Digite o CPF: " << std::endl;
+        std::cin >> cpfinput; // atribuicao do CPF digitado
+       try{ // tentativa da operacao do bloco
+            cpf.setCPF(cpfinput); // envio para validacao
+            std::cout << "CPF valido: " << cpf.getCPF() << std::endl;
+            valido = true;
+        } catch (const DominioException& cpfError) { // no caso de erro, executado 
+           std::cerr << "Erro de validacao: " << cpfError.what() << std::endl; // no caso de excecao, exibe mens de erro contida em DE 
+        }
+        
+    }
+    
+    return 0;
+}
