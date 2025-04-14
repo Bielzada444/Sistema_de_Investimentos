004a73b2eaa90801f47e2531c52d2301d59ee505
henrique505 <henriquedeeealmeida@gmail.com>
2025-04-14 15:56:07 -0300

    Update Dinheiro.h
    Adicao da classe para tratamento de erros, mudanca do tipo em que o input eh recebido, criacao do metodo stringParaDouble(nome autoexplicativo)

--- include/Dinheiro.h
+++ include/Dinheiro.h
@@ -5,15 +5,25 @@
 #ifndef DINHEIRO_H
 #define DINHEIRO_H
 
-
+#include <stdexcept> // Biblioteca onde esta contida a  calsse base runtime_error 
+ 
+ class DominioException : public std::runtime_error { // Classe publica de tratamento de erros
+     public:
+         explicit DominioException(const std::string& mensagem) // Construtor da classe que recebe as mensagens de erro
+         : std::runtime_error(mensagem) {} // Passa a mensagem para o construtor da classe base
+ };
+ 
 class Dinheiro {
       private:
-        double valor;
-        bool validarValor(const double &valor);
-        double arrendodar(const double &valor);
+        double valor; // Armazena o valor do dinheiro como double
+        
+        bool validarValor(const double& valor); // Metodo que faz a validacao do valor digitado
+        
+        double arrendodar(const double& valor); // Metodo que arrendonda o valor para duas casas decimais depois do ponto
+        double stringParaDouble(std::string& valor);// Metodo para converter o input recebido como string para ser armazenado como double
        public:
-         void setValor(const double &valor);
-        inline double getValor()  {
+         void setValor(std::string& valor); // Recebe o input do usuario
+        inline double getValor()  { // Retorna o valor armazenado do atributo 
            return valor;
     }
 };
