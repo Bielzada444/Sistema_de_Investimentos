435b7f86672383710b250f83e952cfdb8a8bd3a0
henrique505 <henriquedeeealmeida@gmail.com>
2025-04-22 11:42:17 -0300

    Update TUQuantidade.h

--- src/testes/headers/TUQuantidade.h
+++ src/testes/headers/TUQuantidade.h
@@ -1,47 +1,23 @@
+#ifndef "TUQantidade.h"
+#define "TUQuantidade.h"
+#include "Quantidade.h"
 
-#include "TUQuantidade.h"
-#include <string>
+class TUQuantidade{
+        private:
+        const static std::string QUANTIDADE_VALIDA; // string usada para teste do QUANTIDADE_VALIDO
+        const static std::string QUANTIADADE_INVALIDO; //  string usada para teste do QUANTIDADE_INVALIDO
+        Quantidade* quantidade; //  ponteiro do objeto Quantidade que sera usado nos testes
+        int estadoQuantidade; // inteiro que ira armazenar o estado de teste, seja SUCESSO ou FALHA
+        
+        void setUpQuantidade(); // inicializa o ambiente de teste
+        void tearDownQuantidade(); // limpa o ambiente de teste
+        void testarCenarioValidoQuantidade(); // testa um Quantidade valido
+        void testarCenarioInvalidoQuantidade(); // testa um Quantidade invalido
+    public:
+        const static int SUCESSO = 0; // inteiro que define sucesso do caso de teste
+        const static int FALHA = -1; // inteiro que define falha do caso de teste
+        int runQuantidade(); // dereciona a execucaco de todos os testes 
 
-
-const std::string TUQuantidade::QUANTIDADE_VALIDO = "150000"; // inicializacao de Quantidade que passa em todos os testes
-const std::string TUQuantidade::QUANTIDADE_INVALIDO = "111111111";// inicializacao de Quantidade que falha na validacao
-
-
-void TUQuantidade::setUpQuantidade() { 
-    quantidade = new Quantidade(); // aloca um novo objeto Quantidade para teste
-    estadoQuantidade = SUCESSO; // incializa o estado para SUCESSO
-}
-
-void TUQuantidade::tearDownQuantidade() {
-    delete quantidade; // libera memoria usado do objeto Quantidade alocado para teste
-}
-
-void TUQuantidade::testarCenarioValidoQuantidade() {
-    try { 
-        quantidade->setQuantidade(QUANTIDADE_VALIDO); // tenta definir um Quantidade valido
-        if( quantidade->getQuantidade() != QUANTIDADE_VALIDO){ // verifica se o valor foi armazenado corretamente
-            estadoQuantidade = FALHA; // se for diferente do esperado marca como FALHA
-        }
-    } catch(DominioException &excecao) { // lanca excecao
-        estadoQuantidade = FALHA; // marca o teste como FALHA
-    }
-}
-
-void TUQuantidade::testarCenarioInvalidoQuantidade() {
-    try {
-        quantidade->setQuantidade(QUANTIDADE_INVALIDO); //tenta definir um Quantidade invalido
-        estadoQuantidade = FALHA; // se conseguir marca como FALHA
-    } catch(DominioException &excecao) { //lanca excecao
-        if(quantidade->getQuantidade() == QUANTIDADE_INVALIDO){ //verifica se o Quantidade invalido nao foi armazenado
-            estadoQuantidade = FALHA; // se foi armazenado marca como FALHA
-        }
-    }
-}
-
-int TUQuantidade::runQuantidade() {
-    setUpQuantidade(); // prepara para o teste
-    testarCenarioValidoQuantidade(); // executa o teste para cenario valido
-    testarCenarioInvalidoQuantidade(); // executa o teste para cenario invalido
-    tearDownQuantidade(); // libera a memoria do objeto alocado para teste
-    return estadoQuantidade; // retorna o resultado final do teste
-}
+    
+};
+#endif 
