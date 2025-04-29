7e06dab11e8afa408255d522afcca03f9963c4f8
henrique505 <henriquedeeealmeida@gmail.com>
2025-04-29 11:14:26 -0300

    Update Dinheiro.hpp

--- include/Dinheiro.hpp
+++ include/Dinheiro.hpp
@@ -1,3 +1,4 @@
+
 //
 // Criado por Henrique em 04/04/2025.
 // Matricula: 241020840
@@ -10,7 +11,7 @@ class Dinheiro {
 private:
 	double valor; // Armazena o valor do dinheiro como double
 
-	bool validarValor(const double& valor); // Metodo que faz a validacao do valor digitado
+	void validarValor(const double& valor); // Metodo que faz a validacao do valor digitado
 
 	double arrendodar(const double& valor); // Metodo que arrendonda o valor para duas casas decimais depois do ponto
 	double stringParaDouble(std::string& valor);// Metodo para converter o input recebido como string para ser armazenado como double
