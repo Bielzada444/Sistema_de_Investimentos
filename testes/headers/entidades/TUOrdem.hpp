4c0dde4a102a8e5e595bdc993966ba23a7665e42
henrique505 <henriquedeeealmeida@gmail.com>
2025-05-08 15:45:47 -0300

    Update TUOrdem.hpp

--- testes/headers/entidades/TUOrdem.hpp
+++ testes/headers/entidades/TUOrdem.hpp
@@ -3,13 +3,15 @@
 
 #include "Ordem.hpp"
 #include "Dominios.hpp"
+#include <string>
 
 class TUOrdem {
+    private:
 	const std::string CODIGO_VALIDO = "12345";
 	const std::string CODIGONEGOCIACAO_VALIDO = "TEST4";
 	const std::string DATA_VALIDA = "20250508";
-	const std::string DINHEIRO_VALIDO = "20863.67";
-	const std::string QUANTIDADE_VALIDA = "150000";
+	std::string DINHEIRO_VALIDO = "20863.67";
+	std::string QUANTIDADE_VALIDA = "150000";
 
 	Ordem* ordem;
 	int estadoOrdem;
@@ -22,7 +24,7 @@ public:
 	const static int SUCESSO = 0;
 	const static int FALHA = -1;
 	int runOrdem();
-
 };
+
 
 #endif // TUORDEM_HPP
