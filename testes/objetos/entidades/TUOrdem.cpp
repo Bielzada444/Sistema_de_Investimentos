2272147991b617accf4069468f1142a69a01b278
henrique505 <henriquedeeealmeida@gmail.com>
2025-05-08 15:46:15 -0300

    Update TUOrdem.cpp

--- testes/objetos/entidades/TUOrdem.cpp
+++ testes/objetos/entidades/TUOrdem.cpp
@@ -17,6 +17,8 @@ void TUOrdem::testarCenarioValidoOrdem() {
     Dinheiro dinheiro;
     Quantidade quantidade;
     
+    int qtd_numero = stoi(QUANTIDADE_VALIDA);
+    double qtd_dinheiro = stod(DINHEIRO_VALIDO);
     codigo.setCodigo(CODIGO_VALIDO);
     codigoNegociacao.setCodigoNegociacao(CODIGONEGOCIACAO_VALIDO);
     data.setData(DATA_VALIDA);
@@ -33,8 +35,8 @@ void TUOrdem::testarCenarioValidoOrdem() {
     if (ordem->getCodigo().getCodigo() != CODIGO_VALIDO ||
         ordem->getCodigoNegociacao().getCodigoNegociacao() != CODIGONEGOCIACAO_VALIDO ||
         ordem->getData().getData() != DATA_VALIDA ||
-        ordem->getDinheiro().getDinheiro() != DINHEIRO_VALIDO ||
-        ordem->getQuantidade().getQuantidade() != QUANTIDADE_VALIDA ) {
+        ordem->getDinheiro().getDinheiro() != qtd_dinheiro ||
+        ordem->getQuantidade().getQuantidade() !=  qtd_numero ) {
         estadoOrdem = FALHA;
     }
     
