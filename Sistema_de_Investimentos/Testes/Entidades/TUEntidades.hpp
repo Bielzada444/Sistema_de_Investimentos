e6391346980070792efad1be14e47d5dca7582f4
henrique505 <henriquedeeealmeida@gmail.com>
2025-05-11 22:53:40 -0300

    Add files via upload

---
+++ Sistema_de_Investimentos/Testes/Entidades/TUEntidades.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Henrique on 11/05/2025.
+//
+
+#ifndef TUENTIDADES_HPP
+#define TUENTIDADES_HPP
+
+#include "TUPerfil.hpp"
+#include "TUCarteira.hpp"
+#include "TUOrdem.hpp"
+#endif //TUENTIDADES_HPP
