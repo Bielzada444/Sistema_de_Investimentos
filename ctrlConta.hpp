8a7efabeca22d05cccbd0ee9704dc4e6090bc8f8
henrique505 <henriquedeeealmeida@gmail.com>
2025-07-06 01:27:58 -0300

    Update ctrlConta.hpp

--- ctrlConta.hpp
+++ ctrlConta.hpp
@@ -1,7 +1,3 @@
-//
-// Created by Henrique on 28/06/2025.
-//
-
 #ifndef CTRLCONTA_HPP_INCLUDED
 #define CTRLCONTA_HPP_INCLUDED
 
@@ -9,32 +5,38 @@
 #include "interfaces.hpp"
 #include "servicoConta.hpp"
 #include "ctrlCarteira.hpp"
-#include "servicoCarteira.hpp"
 #include "ctrlOrdem.hpp"
+#include "servicoCarteira.hpp"
 #include "servicoOrdem.hpp"
+#include "servicoDadosHistoricos.hpp"
 
 class CtrlConta : public IUConta {
 private:
     ILNConta *servico = nullptr;
     CtrlCarteira *ctrlCarteira = nullptr;
+    CtrlOrdem *ctrlOrdem = nullptr;
     ServicoCarteira *servicoCarteira = nullptr;
-    IUOrdem *ctrlOrdem = nullptr;
+    ServicoOrdem *servicoOrdem = nullptr;
+    ServicoDadosHistoricos *servicoDados = nullptr;
 
 public:
     void setCntr(ILNConta *cntr);
     void setCtrlCarteira(CtrlCarteira *cntrCarteira);
+    void setCtrlOrdem(CtrlOrdem *cntrOrdem);
     void setServicoCarteira(ServicoCarteira *servicoCarteira);
-    void setCtrlOrdem(IUOrdem* ctrl);
+    void setServicoOrdem(ServicoOrdem *servicoOrdem);
+    void setServicoDados(ServicoDadosHistoricos *servicoDados);
 
     void menu(const CPF &cpf);
 
     void criar() override;
-    void editar() override {} // não usado diretamente
-    void excluir() override {} // não usado diretamente
+    void editar() override {}     // não usado diretamente
+    void excluir() override {}    // não usado diretamente
     void ler() override;
 
     void editar(const CPF &cpf);
     void excluir(const CPF &cpf);
+    void gerenciarOrdens(const CPF &cpf);
 };
 
 #endif // CTRLCONTA_HPP_INCLUDED
